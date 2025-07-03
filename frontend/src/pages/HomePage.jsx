a9c2f513401844c913b9577217d8bc777be98901
kalevlill <lill.kalev@gmail.com>
2025-07-03 19:02:53 +0200

    reordered files and folders

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react";
+import { useNavigate } from "react-router-dom";
+
+function HomePage() {
+  return ( 
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main page of the application.</p>
+    </div>
+  );
+}
+
+export default HomePage;
