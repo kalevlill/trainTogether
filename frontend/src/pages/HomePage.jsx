b68d66ef06d273f229baa9cb421c7c3a92cf4aba
kalevlill <lill.kalev@gmail.com>
2025-07-29 23:13:45 +0200

    improved the styling of my landing page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,19 +3,21 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../style/HomePage.css";
 import { IoLocation } from "react-icons/io5";
-import { LiaCalendarAlt } from "react-icons/lia";
-import { IoTrophySharp } from "react-icons/io5";
+import { ImUsers } from "react-icons/im";
+import { GiBiceps } from "react-icons/gi";
 import jogging from "../images/jogging.jpg";
 import gym from "../images/gym.jpg";
 import hiking from "../images/hiking.jpg";
+import football from "../images/football.jpg";
 
 function HomePage() {
   const navigate = useNavigate();
 
   const slides = [
-    { src: jogging, text: "Run Together, Achieve More" },
-    { src: gym, text: "Train Smart, Train Together" },
-    { src: hiking, text: "Explore Trails, Build Bonds" },
+    { src: jogging, text: "Push for the Extra Mile, Together" },
+    { src: gym, text: "Push each other through every set, every rep, every workout" },
+    { src: hiking, text: "Reach New Peaks, Side by Side" },
+    { src: football, text: "Your next midfielder is ready to team up" },
   ];
 
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -59,8 +61,8 @@ function HomePage() {
             <span className="yellow-text">Training <br /> Partner</span>
           </h1>
           <p>
-            Connect with local athletes and <br />
-            plan your training together effectively
+            Find training partners nearby <br />
+            and reach your goals together
           </p>
           <button className="get-started-btn" onClick={handleGetStarted}>
             Get Started
@@ -78,15 +80,15 @@ function HomePage() {
       <div className="icon-row">
         <div className="icon-item">
           <IoLocation size={40} />
-          <p>Set Your Location</p>
+          <p>Set your location</p>
         </div>
         <div className="icon-item">
-          <LiaCalendarAlt size={40} />
-          <p>Schedule Your Sessions</p>
+          <ImUsers size={40} />
+          <p>Make new contacts</p>
         </div>
         <div className="icon-item">
-          <IoTrophySharp size={40} />
-          <p>Track Your Progress</p>
+          <GiBiceps size={40} />
+          <p>Get stronger together</p>
         </div>
       </div>
     </>
